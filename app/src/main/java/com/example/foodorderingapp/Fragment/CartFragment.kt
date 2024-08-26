99c9556c960d45cd9267f5e2c1f8b07e7cd9c1cf
Pallavi <pallavi.srivastava_cs22@gla.ac.in>
2024-08-26 12:30:14 +0530

    ...

--- app/src/main/java/com/example/foodorderingapp/Fragment/CartFragment.kt
+++ app/src/main/java/com/example/foodorderingapp/Fragment/CartFragment.kt
@@ -1,5 +1,6 @@
 package com.example.foodorderingapp.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodorderingapp.Adapter.CartAdapter
+import com.example.foodorderingapp.CongratsButtonSheet
+import com.example.foodorderingapp.PayOutActivity
 import com.example.foodorderingapp.R
 import com.example.foodorderingapp.databinding.FragmentCartBinding
 import com.example.foodorderingapp.databinding.FragmentHomeBinding
@@ -35,6 +38,13 @@ private lateinit var binding: FragmentCartBinding
         val adapter = CartAdapter(ArrayList(cartfoodName), ArrayList(cartitemPrice), ArrayList(cartitemImage))
         binding.cartRecyclerview.layoutManager = LinearLayoutManager(requireContext())
         binding.cartRecyclerview.adapter = adapter
+
+        binding.proceedButton.setOnClickListener{
+            val intent= Intent(requireContext(), PayOutActivity::class.java)
+            startActivity(intent)
+        }
+
+
         return binding.root
 
 
---
+++ app/src/main/java/com/example/foodorderingapp/PayOutActivity.kt
@@ -0,0 +1,27 @@
+package com.example.foodorderingapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.foodorderingapp.databinding.ActivityPayOutBinding
+
+class PayOutActivity : AppCompatActivity() {
+    lateinit var binding: ActivityPayOutBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPayOutBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.PlaceMyOrder.setOnClickListener{
+            val bottomSheetDialog=CongratsButtonSheet()
+            bottomSheetDialog.show(supportFragmentManager,"Test")
+        }
+    }
+}
